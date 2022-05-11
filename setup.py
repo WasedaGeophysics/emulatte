8ba708a509cb5cced2d543472cea32d4aa9abdc3
tnishino <niche240tamaki@gmail.com>
2022-05-12 08:15:34 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -14,69 +14,25 @@
 
 from setuptools import setup, find_packages
 
+PYTHON_REQUIRES = '>=3.6'
+
+with open('README.md', 'r', encoding='utf-8') as fp:
+    readme = fp.read()
+LONG_DESCRIPTION = readme
+LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'
+
 setup(
     name = 'emulatte',
     version = '0.1.0',
     description = 'A Primitive Tools for Electromagnetic Explorations',
-    long_description = 'README',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,\
     author = 'Takumi Ueda',
     author_email = 'takumi.ueda@waseda.jp',
     url = 'https://github.com/WasedaGeophysics/emulatte.git',
+    download_url = 'https://github.com/WasedaGeophysics/emulatte.git',
     license = license("Apache2.0"),
-    packages = find_packages(exclude=('docs', 'tests', 'tutorials')),
+    keywords = 'electromagnetics geophysics EM 1DEM'
+    packages = find_packages(exclude=('docs', 'lab', 'example', 'analytical')),
     install_requires = open('requirements.txt').read().splitlines(),
-)
-
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-from __future__ import absolute_import
-from __future__ import unicode_literals
-import os
-
-from setuptools import setup, find_packages
-
-try:
-    with open('README.rst') as f:
-        readme = f.read()
-except IOError:
-    readme = ''
-
-def _requires_from_file(filename):
-    return open(filename).read().splitlines()
-
-# version
-here = os.path.dirname(os.path.abspath(__file__))
-version = next((line.split('=')[1].strip().replace("'", '')
-                for line in open(os.path.join(here,
-                                              'pypipkg',
-                                              '__init__.py'))
-                if line.startswith('__version__ = ')),
-               '0.0.dev0')
-
-setup(
-    name="pypipkg",
-    version=version,
-    url='https://github.com/user/pypipkg',
-    author='kinpira',
-    author_email='kinpira@example.jp',
-    maintainer='kinpira',
-    maintainer_email='kinpira@example.jp',
-    description='Package Dependency: Validates package requirements',
-    long_description=readme,
-    packages=find_packages(),
-    install_requires=_requires_from_file('requirements.txt'),
-    license="MIT",
-    classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'License :: OSI Approved :: MIT License',
-    ],
-    entry_points="""
-      # -*- Entry points: -*-
-      [console_scripts]
-      pkgdep = pypipkg.scripts.command:main
-    """,
 )
